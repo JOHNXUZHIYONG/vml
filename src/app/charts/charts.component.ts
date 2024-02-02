7dabe30c52fbfe7375ce0f1ec62412aada8b83a5
XuZhiyong <xu_zhiyong@artc.a-star.edu.sg>
2024-02-02 17:11:59 +0800

    before go home

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -9,7 +9,7 @@ import { ChartData, ChartOptions } from 'chart.js';
   styleUrls: ['./charts.component.css']
 })
 export class ChartsComponent implements OnChanges {
-  @Input() chartTitle: string = 'Line Chart'; // 折线图标题
+  @Input() chartTitle: string = ''; // 折线图标题
   @Input() chartData: any[] = []; // 折线图数据
   @Input() chartLabel: any[] = []; // 折线图数据
   @Input() chartType: 'line' | 'bar' = 'line';
@@ -24,12 +24,14 @@ ngOnChanges(changes: SimpleChanges): void {
 
 
   dataBar: ChartData<'bar'> = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+
+    labels: ['Part 1', 'Part 2', 'Part 3', 'Part 4', 'Part 5'],
     datasets: [
-      { label: 'Mobiles', data: [1000, 1200, 1050, 2000, 500] },
-      { label: 'Laptop', data: [200, 100, 400, 50, 90] },
-      { label: 'AC', data: [500, 400, 350, 450, 650] },
-      { label: 'Headset', data: [1200, 1500, 1020, 1600, 900] },
+      // { label: 'Stop', data: [1000, 1200, 1050, 2000, 500] },
+      { label: 'Part Prodction Time /s', data: [110, 100, 120, 100, 90],     borderColor: 'blue',
+      backgroundColor: 'aqua',},
+      // { label: 'AC', data: [500, 400, 350, 450, 650] },
+      // { label: 'Run', data: [1200, 1500, 1020, 1600, 900] },
     ],
     
   };
@@ -39,10 +41,36 @@ ngOnChanges(changes: SimpleChanges): void {
     plugins: {
       title: {
         display: true,
-        text: 'Monthly Sales Data',
+        text: 'Part Production Time',
+        color: 'green', // 设置字体颜色
+      font: {
+        size: 16, // 设置字体大小
+        weight: 'bold', // 设置字体粗细，可以是 'normal' 或 'bold'
+        family: 'Arial, sans-serif', // 设置字体族
+        
+      }
       },
+      legend: {
+        // display: false, // 设置为 false 取消图例
+        position: "bottom"
+      },
+      
     },
-    
+    scales: {
+      x: {
+        ticks: {
+          color: 'white', // 设置 x 轴刻度文字颜色
+        }
+      },
+      y: {
+        ticks: {
+          color: 'white', // 设置 y 轴刻度文字颜色
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.2)' // 设置 x 轴网格线颜色
+        }
+      }
+    }
   };
 
 
@@ -56,14 +84,21 @@ ngOnChanges(changes: SimpleChanges): void {
 
 chartOptionsLine: ChartOptions = {
   responsive: true,
-  maintainAspectRatio: false, // 禁用纵横比维持
-    aspectRatio: 2, // 设置宽高比例，只在 maintainAspectRatio 为 false 时生效
+  // maintainAspectRatio: false, // 禁用纵横比维持
+  //   aspectRatio: 1.5, // 设置宽高比例，只在 maintainAspectRatio 为 false 时生效
     
   plugins: {
     title: {
       display: true,
-      text: 'Monthly Sales Data',
-      // color: "red" 
+      text: 'HAAS Power Consumption',
+      color: 'green', // 设置字体颜色
+      font: {
+        size: 16, // 设置字体大小
+        weight: 'bold', // 设置字体粗细，可以是 'normal' 或 'bold'
+        family: 'Arial, sans-serif', // 设置字体族
+        
+      }
+      
       
     },
     legend: {
@@ -76,14 +111,34 @@ chartOptionsLine: ChartOptions = {
     },
     
   },
+  scales: {
+    x: {
+      ticks: {
+        color: 'white', // 设置 x 轴刻度文字颜色
+      },
+      grid: {
+        color: 'rgba(255, 255, 255, 0.2)' // 设置 x 轴网格线颜色
+      }
+    },
+    y: {
+      ticks: {
+        color: 'white', // 设置 y 轴刻度文字颜色
+      },
+      grid: {
+        color: 'rgba(255, 255, 255, 0.2)' // 设置 x 轴网格线颜色
+      },
+      suggestedMin: 0.322, // 设置 y 轴的最小值
+      suggestedMax: 0.325, // 设置 y 轴的最大值
+    }
+  }
 };
 
 // 在组件类中定义一个方法，用于更新数据
 private updateChartData(): void {
   if (this.chartLabel && this.chartData) {
     // 假设chartData的结构与初始数据相同
-    this.dataBar.datasets = this.chartData;
-    this.dataBar.labels = this.chartLabel;
+    // this.dataBar.datasets = this.chartData;
+    // this.dataBar.labels = this.chartLabel;
     this.dataLine.datasets = this.chartData;
     this.dataLine.labels = this.chartLabel;
   }
