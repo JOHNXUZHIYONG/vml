0ba2139ee7c4eb088825466c3634b7a3f1513716
XuZhiyong <xu_zhiyong@artc.a-star.edu.sg>
2024-03-06 00:24:20 +0800

    simplify charts component

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -15,126 +15,57 @@ export class ChartsComponent implements OnChanges {
   @Input() chartLabel: any[] = []; // 折线图数据
   @Input() chartType: 'line' | 'bar' = 'line';
   @Input() pTotalTimeList: any[] = [];
+  @Input() chartOptionsBar: any;
+  @Input() chartOptionsLine: any;
+  @Input() dataBar: any;
+  @Input() dataLine: any;
+
   @ViewChild(BaseChartDirective) chart!: BaseChartDirective; // 使用非空断言
 
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if ((changes['chartLabel'] || changes['chartData']) && this.chart) {
-      this.updateChartData();
+    // if (true) {
+    //   // this.updateChartData();
       this.chart.update(); // 手动触发图表更新
-    }
+    // }
   }
 
 
-  dataBar: ChartData<'bar'> = {
+ 
+  // dataBar: ChartData<'bar'> = {
 
-    labels: [1,2,3,4,5,6,7,8,9,10],
-    datasets: [ { "label": "Part Prodction Time /s", "data": [ 5, 5, 6, 6, 5, 6, 6, 5, 5, 6 ], "borderColor": "blue", "backgroundColor": "aqua" } ],
+  //   labels: [],
+  //   datasets: [],
 
-  };
-  chartOptionsBar: ChartOptions = {
-    responsive: true,
-
-    plugins: {
-      title: {
-        display: true,
-        text: 'Part Production Time',
-        color: 'green', // 设置字体颜色
-        font: {
-          size: 16, // 设置字体大小
-          weight: 'bold', // 设置字体粗细，可以是 'normal' 或 'bold'
-          family: 'Arial, sans-serif', // 设置字体族
-
-        }
-      },
-      legend: {
-        // display: false, // 设置为 false 取消图例
-        position: "bottom"
-      },
-
-    },
-    scales: {
-      x: { ticks: { color: 'white',  }},   // 设置 x 轴刻度文字颜色
-      y: {ticks: {  color: 'white',},      // 设置 y 轴刻度文字颜色   
-      grid: { color: 'rgba(255, 255, 255, 0.2)'}} // 设置 x 轴网格线颜色
-    }
-  };
+  // };
 
 
-  dataLine: ChartData<'line'> = {
-    labels: ["55-60", "50-55", "45-50", "40-45", "35-40", "30-35", "25-30", "20-25", "15-20", "10-15",
-    "5-10", "0-5"],
+  // dataLine: ChartData<'line'> = {
+  //   labels: ["55-60", "50-55", "45-50", "40-45", "35-40", "30-35", "25-30", "20-25", "15-20", "10-15",
+  //   "5-10", "0-5"],
 
-    datasets:  [ { "label": "P_total", "data": [ 80, 77.15, 94.86, 80, 94.86, 94.86, 80, 94.86, 71.15, 80, 80, 90 ], "tension": 0.5, "borderColor": "green", "backgroundColor": "lightgreen" }],
+  //   datasets:  [ { "label": "P_total", "data": [ 80, 77.15, 94.86, 80, 94.86, 94.86, 80, 94.86, 71.15, 80, 80, 90 ], "tension": 0.5, "borderColor": "green", "backgroundColor": "lightgreen" }],
     
 
-  };
+  // };
 
 
-  chartOptionsLine: ChartOptions = {
-    responsive: true,
-    // maintainAspectRatio: false, // 禁用纵横比维持
-    //   aspectRatio: 1.5, // 设置宽高比例，只在 maintainAspectRatio 为 false 时生效
-
-    plugins: {
-      title: {
-        display: true,
-        text: 'HAAS Power Consumption',
-        color: 'green', // 设置字体颜色
-        font: {
-          size: 16, // 设置字体大小
-          weight: 'bold', // 设置字体粗细，可以是 'normal' 或 'bold'
-          family: 'Arial, sans-serif', // 设置字体族
-
-        }
-
-
-      },
-      legend: {
-        // display: false, // 设置为 false 取消图例
-        position: "bottom"
-      },
-      tooltip: {
-        mode: 'index',
-        intersect: false,
-      },
-
-    },
-    scales: {
-      x: {
-        ticks: {
-          color: 'white', // 设置 x 轴刻度文字颜色
-        },
-        grid: {
-          color: 'rgba(255, 255, 255, 0.2)' // 设置 x 轴网格线颜色
-        }
-      },
-      y: {
-        ticks: {
-          color: 'white', // 设置 y 轴刻度文字颜色
-        },
-        grid: {
-          color: 'rgba(255, 255, 255, 0.2)' // 设置 x 轴网格线颜色
-        },
-        // suggestedMin: 0.322, // 设置 y 轴的最小值
-        // suggestedMax: 0.325, // 设置 y 轴的最大值
-      }
-    }
-  };
 
   // 在组件类中定义一个方法，用于更新数据
-  private updateChartData(): void {
-    console.log(this.chartData);
-    if (this.chartData) {
-      this.dataLine.datasets = this.chartData;
-      this.dataLine.labels = this.chartLabel;
-      // 假设chartData的结构与初始数据相同
-      this.dataBar.datasets = this.chartData;
-      this.dataBar.labels = this.chartLabel;
+  // private updateChartData(): void {
+    
+  //   if (this.dataBar || this.dataLine) {
+  //     // this.dataLine.datasets = this.chartData;
+  //     // this.dataLine.labels = this.chartLabel;
+  //     // 假设chartData的结构与初始数据相同
+  //     // this.dataBar.datasets = this.chartData;
+  //     // this.dataBar.labels = this.chartLabel;
+  //     this.dataBar = this.dataBar;
+  //     this.dataLine=this.dataLine;
 
-    }
-  }
+  //   }
+  // }
 }
 
 
--- src/app/haas/haas.component.ts
+++ src/app/haas/haas.component.ts
@@ -2,12 +2,8 @@ import { Component } from '@angular/core';
 import { DataService } from '../data.service';
 import { catchError, interval, of, startWith, switchMap } from 'rxjs';
 import * as moment from 'moment';
+import { ChartData, ChartOptions } from 'chart.js';
 
-interface HaasPartTimeEntry {
-  time: string;
-  part_number: string;
-  // 其他可能的属性...
-}
 
 @Component({
   selector: 'app-haas',
@@ -15,6 +11,22 @@ interface HaasPartTimeEntry {
   styleUrls: ['./haas.component.css']
 })
 export class HaasComponent {
+  dataLine: ChartData<'line'> = {
+    labels: ["55-60", "50-55", "45-50", "40-45", "35-40", "30-35", "25-30", "20-25", "15-20", "10-15",
+    "5-10", "0-5"],
+
+    datasets:  [ { "label": "P_total", "data": [ 80, 77.15, 94.86, 80, 94.86, 94.86, 80, 94.86, 71.15, 80, 80, 90 ], "tension": 0.5, "borderColor": "green", "backgroundColor": "lightgreen" }],
+    
+
+  };
+
+
+  dataBar: ChartData<'bar'> = {
+
+    labels: [1,2,3,4,5,6,7,8,9,10],
+    datasets: [ { "label": "Part Prodction Time /s", "data": [ 5, 5, 6, 6, 5, 6, 6, 5, 5, 6 ], "borderColor": "blue", "backgroundColor": "aqua" } ],
+
+  };
 
   data: any;
   haasPowermeterData: any;
@@ -26,7 +38,8 @@ export class HaasComponent {
 
   barChartData: any[] = [];
   partList: number[] = [];
-  
+
+ 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
@@ -69,7 +82,9 @@ export class HaasComponent {
 
         // 仅在两个列表都有数据时更新 lineChartData
         this.barChartData = chartData1;
-        this.partList = this.partTimeData[0];
+        // this.partList = this.partTimeData[0];
+        this.dataBar.datasets=chartData1;
+        // this.dataBar.labels=this.partTimeData[0];
       },
       (error) => {
         console.error('Error fetching part time data:', error);
@@ -91,12 +106,134 @@ export class HaasComponent {
         }];
 
         this.lineChartData = chartData;
+        this.dataLine.datasets = chartData;
       },
       (error) => {
         console.error('Error fetching Powermeter data:', error);
       }
     );
   }
+
+  chartOptionsLine: ChartOptions = {
+    responsive: true,
+    // maintainAspectRatio: false, // 禁用纵横比维持
+    //   aspectRatio: 1.5, // 设置宽高比例，只在 maintainAspectRatio 为 false 时生效
+
+    plugins: {
+      title: {
+        display: true,
+        text: 'HAAS Power Consumption (Past 1 Hour)',
+        color: 'green', // 设置字体颜色
+        font: {
+          size: 16, // 设置字体大小
+          weight: 'bold', // 设置字体粗细，可以是 'normal' 或 'bold'
+          family: 'Arial, sans-serif', // 设置字体族
+
+        }
+
+
+      },
+      legend: {
+        display: false, // 设置为 false 取消图例
+        position: "bottom"
+      },
+      tooltip: {
+        mode: 'index',
+        intersect: false,
+      },
+
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Time Interval / Per 5 Minutes',
+          color: 'white', // 设置 x 轴标题文字颜色
+          font: {
+            size: 14,
+            weight: 'bold',
+            family: 'Arial, sans-serif',
+          }
+        },
+        ticks: {
+          color: 'white', // 设置 x 轴刻度文字颜色
+          
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.2)' // 设置 x 轴网格线颜色
+        }
+        
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Energy Usage / kWh',
+          color: 'white', // 设置 y 轴标题文字颜色
+          font: {
+            size: 14,
+            weight: 'bold',
+            family: 'Arial, sans-serif',
+          }
+        },
+        ticks: {
+          color: 'white', // 设置 y 轴刻度文字颜色
+        },
+        grid: {
+          color: 'rgba(255, 255, 255, 0.2)' // 设置 x 轴网格线颜色
+        },
+        // suggestedMin: 0.322, // 设置 y 轴的最小值
+        // suggestedMax: 0.325, // 设置 y 轴的最大值
+      }
+    }
+  };
+  
+  chartOptionsBar: ChartOptions = {
+    responsive: true,
+
+    plugins: {
+      title: {
+        display: true,
+        text: 'Part Production Cycle Time',
+        color: 'green', // 设置字体颜色
+        font: {
+          size: 16, // 设置字体大小
+          weight: 'bold', // 设置字体粗细，可以是 'normal' 或 'bold'
+          family: 'Arial, sans-serif', // 设置字体族
+
+        }
+      },
+      legend: {
+        display: false, // 设置为 false 取消图例
+        position: "bottom"
+      },
+
+    },
+    scales: {
+      x: { ticks: { color: 'white',  }, title: {
+        display: true,
+        text: 'Part Number',
+        color: 'white', // 设置 x 轴标题文字颜色
+        font: {
+          size: 14,
+          weight: 'bold',
+          family: 'Arial, sans-serif',
+        }
+      },},   // 设置 x 轴刻度文字颜色
+      y: {ticks: {  color: 'white', }, 
+      title: {
+        display: true,
+        text: 'Production Time / s',
+        color: 'white', // 设置 y 轴标题文字颜色
+        font: {
+          size: 14,
+          weight: 'bold',
+          family: 'Arial, sans-serif',
+        }
+      },     // 设置 y 轴刻度文字颜色   
+      grid: { color: 'rgba(255, 255, 255, 0.2)'}} // 设置 x 轴网格线颜色
+    }
+  };
+  
 
 }
 
